23cf80d820a04f34280ef64747de0bc29d548534
Jerry Hu <work.jerrywu@gmail.com>
2025-01-18 12:40:39 -0800

    added files

--- db/factory.py
+++ db/factory.py
@@ -19,8 +19,8 @@ class MongoJsonEncoder(JSONEncoder):
 def create_app():
 
     APP_DIR = os.path.abspath(os.path.dirname(__file__))
-    STATIC_FOLDER = os.path.join(APP_DIR, 'build/static')
-    TEMPLATE_FOLDER = os.path.join(APP_DIR, 'build')
+    STATIC_FOLDER = os.path.(join(APP_DIR, )'build/static')
+    TEMPLATE_FOLDER = os.path('../templates')
 
     app = Flask(__name__, static_folder=STATIC_FOLDER,
                 template_folder=TEMPLATE_FOLDER,
